ce5855d6ba09b0f6c2d528e725d33757a26e9079
MiriamLongoyapus <longoyapusmiriam@gmail.com>
2023-03-29 19:28:50 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtract(x,y):
+    answer=x-y
+    return answer
+
+def divide(number1,number2):
+    answer=number1/number2
+    return answer
+
+def multiply(c,d):
+    answer=c*d
+    return answer
+
+def remainder(z,y):
+    answer=z%y
+    return answer        
